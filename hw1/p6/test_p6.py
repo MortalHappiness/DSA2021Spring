56cf29170df72dcd66ca751c077e38816bdda853
MortalHappiness <b07901069@ntu.edu.tw>
2021-03-29 21:29:31 +0800

    Update hw1/p6 testcases

--- hw1/p6/test_p6.py
+++ hw1/p6/test_p6.py
@@ -29,6 +29,15 @@ class TestP6(unittest.TestCase):
     def test_3_sample(self):
         self.run_testcase(3)
 
+    def test_4_only_query(self):
+        self.run_testcase(4)
+
+    def test_5_no_query(self):
+        self.run_testcase(5)
+
+    def test_6_mix(self):
+        self.run_testcase(6)
+
 
 if __name__ == "__main__":
     unittest.main()
